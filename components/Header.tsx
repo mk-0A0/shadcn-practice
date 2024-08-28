7ac36e6d2477fff41ee99440c9f6b910d308d821
momoka <moowfaw@gmail.com>
2024-08-28 23:02:02 +0900

    add: Header component

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+
+export default function Header() {
+  return (
+    <header>
+      <span>ロゴ</span>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton>
+          <Button variant="outline">ログイン</Button>
+        </SignInButton>
+        <SignUpButton>
+          <Button>新規登録</Button>
+        </SignUpButton>
+      </SignedOut>
+    </header>
+  )
+}
