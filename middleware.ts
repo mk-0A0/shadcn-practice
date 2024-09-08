45c26b0e086bbb4b4c3c7409dbb86104027a98ce
momoka <moowfaw@gmail.com>
2024-09-08 11:31:20 +0900

    feat: 未ログイン時の処理を追加

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,9 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoutes = createRouteMatcher('/')
 
 export default clerkMiddleware((auth, request) => {
+  // 未ログインかつisPublicRoutesの場合はreturn
+  if (!auth().userId && isPublicRoutes(request)) return
+
   if (!isPublicRoutes(request)) auth().protect()
 })
 
