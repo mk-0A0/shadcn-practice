da37f22dea3ef898d802e75fffccc6d163324fef
momoka <moowfaw@gmail.com>
2024-09-08 23:23:49 +0900

    feat: 記事一覧

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-import Header from '@/components/Header'
+import Link from 'next/link'
+import { db } from '@/lib/prisma'
 
-export default function Home() {
-  return <div></div>
+export default async function Home() {
+  const posts = await db.post.findMany()
+
+  return (
+    <div>
+      {posts.length > 0 ? (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link href={`posts/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>記事はありません</p>
+      )}
+    </div>
+  )
 }
