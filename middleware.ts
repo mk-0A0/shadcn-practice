e1a156f8aba937d659e9ad7122c963ec6d9fb378
momoka <moowfaw@gmail.com>
2024-09-08 11:18:03 +0900

    feat: /以外は認証されていないとリダイレクトする

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+const isUnProtectedRoute = createRouteMatcher('/')
+
+export default clerkMiddleware((auth, request) => {
+  if (!isUnProtectedRoute(request)) auth().protect()
+})
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
