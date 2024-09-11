c02becfb81fa58de6d62ad365a03bd9f99b1c93b
momoka <moowfaw@gmail.com>
2024-09-11 09:04:08 +0900

    feat: 編集するpostを取得してPostFormに渡す

--- app/posts/[id]/edit/page.tsx
+++ app/posts/[id]/edit/page.tsx
@@ -1,5 +1,18 @@
 import PostForm from '@/app/components/post-form'
+import { db } from '@/lib/prisma'
+import { notFound } from 'next/navigation'
+import { authGuard } from '@/app/actions/auth'
 
-export default function Page() {
-  return <PostForm />
+export default function Page({ params: { id } }: { params: { id: string } }) {
+  const authorId = authGuard()
+  const post = db.post.findUnique({
+    where: {
+      authorId,
+      id,
+    },
+  })
+
+  if (!post) notFound()
+
+  return <PostForm editId={id} />
 }
