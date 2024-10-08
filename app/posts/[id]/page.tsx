e039a5835943ff4bc5a9b1022b672e9889f27eaa
momoka <moowfaw@gmail.com>
2024-10-08 09:12:49 +0900

    feat: Metadataを動的に変更

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -2,6 +2,38 @@ import { db } from '@/lib/prisma'
 import { notFound } from 'next/navigation'
 import Link from 'next/link'
 import { currentUser } from '@/app/onboarding/action'
+import { Metadata, ResolvingMetadata } from 'next'
+import { getPost } from '@/app/components/post-form/action'
+
+type Props = {
+  params: { id: string }
+}
+
+export const generateMetadata = async (
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> => {
+  const doc = await getPost(params.id)
+  const parentMeta = await parent
+
+  if (!doc) {
+    return {}
+  }
+
+  return {
+    title: doc.title,
+    description: parentMeta.description,
+    openGraph: {
+      ...parentMeta.openGraph,
+      title: doc.title,
+      description: parentMeta.description || '',
+      // TODO 型エラー解消する
+      //  TS2322: Type "article" is not assignable to type "video.other"
+      // @ts-ignore
+      type: 'article',
+    },
+  }
+}
 
 export default async function Page({
   params: { id },
